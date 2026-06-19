9faf8620f88bbbfc84605c26ae5b3c3ddaebd5d2
Daweidie <3770366940@qq.com>
2026-06-19 12:13:44 +0000

    feat(vixc0) update if elif else statement and fix some bug feat tests for vixc0

--- vixc0/helper.c
+++ vixc0/helper.c
@@ -9,10 +9,20 @@
 typedef struct {
     char name[64];
     LLVMValueRef value;
+    char type[16];
 } VarEntry;
+
+typedef struct {
+    char name[64];
+    char return_type[16];
+    char param_types[16][16];
+    int param_count;
+} FunctionEntry;
 
 static VarEntry vars[MAX_VARS];
 static int var_count = 0;
+static FunctionEntry funcs[MAX_VARS];
+static int func_count = 0;
 
 void vix_reset_vars(void) {
     var_count = 0;
@@ -23,8 +33,29 @@ void vix_set_var(const char *name, LLVMValueRef value) {
         strncpy(vars[var_count].name, name, 63);
         vars[var_count].name[63] = '\0';
         vars[var_count].value = value;
+        vars[var_count].type[0] = '\0';
         var_count++;
     }
+}
+
+void vix_set_var_type(const char *name, const char *type) {
+    for (int i = var_count - 1; i >= 0; i--) {
+        if (strcmp(vars[i].name, name) == 0) {
+            strncpy(vars[i].type, type, 15);
+            vars[i].type[15] = '\0';
+            return;
+        }
+    }
+}
+
+const char *vix_get_var_type(const char *name) {
+    for (int i = var_count - 1; i >= 0; i--) {
+        if (strcmp(vars[i].name, name) == 0) {
+            if (vars[i].type[0] == '\0') return "i32";
+            return vars[i].type;
+        }
+    }
+    return "i32";
 }
 
 LLVMValueRef vix_get_var(const char *name) {
@@ -35,6 +66,66 @@ LLVMValueRef vix_get_var(const char *name) {
     }
     return NULL;
 }
+
+void vix_reset_function_sigs(void) {
+    func_count = 0;
+}
+
+void vix_register_function_sig(const char *name, const char *return_type,
+                               const char **param_types, int param_count) {
+    int idx = -1;
+    for (int i = 0; i < func_count; i++) {
+        if (strcmp(funcs[i].name, name) == 0) {
+            idx = i;
+            break;
+        }
+    }
+    if (idx < 0) {
+        if (func_count >= MAX_VARS) return;
+        idx = func_count++;
+    }
+    strncpy(funcs[idx].name, name, 63);
+    funcs[idx].name[63] = '\0';
+    strncpy(funcs[idx].return_type, return_type, 15);
+    funcs[idx].return_type[15] = '\0';
+    if (param_count > 16) param_count = 16;
+    funcs[idx].param_count = param_count;
+    for (int i = 0; i < param_count; i++) {
+        strncpy(funcs[idx].param_types[i], param_types[i], 15);
+        funcs[idx].param_types[i][15] = '\0';
+    }
+}
+
+const char *vix_get_function_return_type(const char *name) {
+    for (int i = func_count - 1; i >= 0; i--) {
+        if (strcmp(funcs[i].name, name) == 0) {
+            return funcs[i].return_type;
+        }
+    }
+    return "i32";
+}
+
+const char *vix_get_function_param_type(const char *name, int index) {
+    for (int i = func_count - 1; i >= 0; i--) {
+        if (strcmp(funcs[i].name, name) == 0) {
+            if (index >= 0 && index < funcs[i].param_count) {
+                return funcs[i].param_types[index];
+            }
+            return "i32";
+        }
+    }
+    return "i32";
+}
+
+int vix_get_function_param_count(const char *name) {
+    for (int i = func_count - 1; i >= 0; i--) {
+        if (strcmp(funcs[i].name, name) == 0) {
+            return funcs[i].param_count;
+        }
+    }
+    return 0;
+}
+
 LLVMTypeRef vix_LLVMFunctionType0(LLVMTypeRef ret_ty, int is_var_arg) {
     return LLVMFunctionType(ret_ty, NULL, 0, is_var_arg);
 }
@@ -58,8 +149,18 @@ LLVMTypeRef vix_LLVMFunctionTypeI32N(LLVMTypeRef ret_ty, unsigned param_count, i
     return LLVMFunctionType(ret_ty, params, param_count, is_var_arg);
 }
 
+LLVMTypeRef vix_LLVMFunctionTypeTypedN(LLVMTypeRef ret_ty, LLVMTypeRef *params,
+                                       unsigned param_count, int is_var_arg) {
+    if (param_count > 256) param_count = 256;
+    return LLVMFunctionType(ret_ty, params, param_count, is_var_arg);
+}
+
 LLVMValueRef vix_LLVMConstInt(LLVMTypeRef ty, long long val, int sign_extend) {
     return LLVMConstInt(ty, (unsigned long long)val, sign_extend);
+}
+
+LLVMValueRef vix_LLVMConstReal(LLVMTypeRef ty, double val) {
+    return LLVMConstReal(ty, val);
 }
 
 LLVMValueRef vix_LLVMAddFunction(LLVMModuleRef m, const char *name, LLVMTypeRef ty) {
@@ -143,8 +244,28 @@ LLVMValueRef vix_LLVMBuildSRem(LLVMBuilderRef builder, LLVMValueRef l, LLVMValue
     return LLVMBuildSRem(builder, l, r, name);
 }
 
+LLVMValueRef vix_LLVMBuildFAdd(LLVMBuilderRef builder, LLVMValueRef l, LLVMValueRef r, const char *name) {
+    return LLVMBuildFAdd(builder, l, r, name);
+}
+
+LLVMValueRef vix_LLVMBuildFSub(LLVMBuilderRef builder, LLVMValueRef l, LLVMValueRef r, const char *name) {
+    return LLVMBuildFSub(builder, l, r, name);
+}
+
+LLVMValueRef vix_LLVMBuildFMul(LLVMBuilderRef builder, LLVMValueRef l, LLVMValueRef r, const char *name) {
+    return LLVMBuildFMul(builder, l, r, name);
+}
+
+LLVMValueRef vix_LLVMBuildFDiv(LLVMBuilderRef builder, LLVMValueRef l, LLVMValueRef r, const char *name) {
+    return LLVMBuildFDiv(builder, l, r, name);
+}
+
 LLVMValueRef vix_LLVMBuildNeg(LLVMBuilderRef builder, LLVMValueRef val, const char *name) {
     return LLVMBuildNeg(builder, val, name);
+}
+
+LLVMValueRef vix_LLVMBuildFNeg(LLVMBuilderRef builder, LLVMValueRef val, const char *name) {
+    return LLVMBuildFNeg(builder, val, name);
 }
 
 LLVMValueRef vix_LLVMBuildRet(LLVMBuilderRef builder, LLVMValueRef val) {
@@ -160,9 +281,29 @@ LLVMValueRef vix_LLVMBuildICmp(LLVMBuilderRef builder, LLVMIntPredicate op,
     return LLVMBuildICmp(builder, op, l, r, name);
 }
 
+LLVMValueRef vix_LLVMBuildFCmp(LLVMBuilderRef builder, LLVMRealPredicate op,
+                                LLVMValueRef l, LLVMValueRef r, const char *name) {
+    return LLVMBuildFCmp(builder, op, l, r, name);
+}
+
 LLVMValueRef vix_LLVMBuildZExt(LLVMBuilderRef builder, LLVMValueRef val,
                                 LLVMTypeRef dest_ty, const char *name) {
     return LLVMBuildZExt(builder, val, dest_ty, name);
+}
+
+LLVMValueRef vix_LLVMBuildSIToFP(LLVMBuilderRef builder, LLVMValueRef val,
+                                  LLVMTypeRef dest_ty, const char *name) {
+    return LLVMBuildSIToFP(builder, val, dest_ty, name);
+}
+
+LLVMValueRef vix_LLVMBuildFPExt(LLVMBuilderRef builder, LLVMValueRef val,
+                                 LLVMTypeRef dest_ty, const char *name) {
+    return LLVMBuildFPExt(builder, val, dest_ty, name);
+}
+
+LLVMValueRef vix_LLVMBuildFPTrunc(LLVMBuilderRef builder, LLVMValueRef val,
+                                   LLVMTypeRef dest_ty, const char *name) {
+    return LLVMBuildFPTrunc(builder, val, dest_ty, name);
 }
 
 LLVMValueRef vix_LLVMBuildBr(LLVMBuilderRef builder, LLVMValueRef dest) {
