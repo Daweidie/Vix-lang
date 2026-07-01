0caeb6ed986dedeb105213e44f8deabaa218d077
Daweidie <3770366940@qq.com>
2026-07-01 20:17:44 +0800

    feat(bootstrap) add warnning tips for bootstrap compiler

--- bootstrap/build.sh
+++ bootstrap/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./vixc2 main.vix > main.ll
-clang -c main.ll -o vixc1.o
-clang -o vixc3 vixc1.o helper.o $(llvm-config --ldflags --libs)
+set -e
+./vixc1 -obj main.vix -o vixc1.o
+clang -o vixc2 vixc1.o helper.o $(llvm-config --ldflags --libs)
