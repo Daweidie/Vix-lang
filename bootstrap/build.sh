b38363aa6dcda28003b429b28d8c705cee49769a
Daweidie <3770366940@qq.com>
2026-07-02 17:30:19 +0800

    fix(bootstrap) 修复了vixc0无法正常编译的问题

--- bootstrap/build.sh
+++ bootstrap/build.sh
@@ -2,5 +2,5 @@
 set -e
 gcc -c src/helper.c -o helper.o $(llvm-config --cflags)
 gcc -c src/runtime.c -o runtime.o
-./vixc0 -obj src/main.vix -o vixc1.o
-clang -o vixc1 vixc1.o helper.o runtime.o $(llvm-config --ldflags --libs)
+./vixc -obj src/main.vix -o vixc0.o
+clang -o vixc0 vixc0.o helper.o runtime.o $(llvm-config --ldflags --libs)
