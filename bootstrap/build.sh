b81dce2e87ff66f75f46e8095f5b4971b2a25273
Daweidie <3770366940@qq.com>
2026-07-02 21:16:28 +0800

    feat(bootstrap) 支持宏语法，并自举测试

--- bootstrap/build.sh
+++ bootstrap/build.sh
@@ -2,5 +2,5 @@
 set -e
 gcc -c src/helper.c -o helper.o $(llvm-config --cflags) -Wno-deprecated-declarations
 gcc -c src/runtime.c -o runtime.o
-./vixc -obj src/main.vix -o vixc0.o
+./vixc src/main.vix -obj -o vixc0.o
 clang -o vixc0 vixc0.o helper.o runtime.o $(llvm-config --ldflags --libs)
